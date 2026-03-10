c1117c4ea3c98f5325a02ba7eb027efccedb22a5
rifardin <fkfardin900@gmail.com>
2026-03-11 05:17:13 +0600

    first attempt on home

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
-document.getElementById('loginForm').addEventListener('submit', function() {
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    
+    event.preventDefault(); 
 
-            const user = document.getElementById('username').value;
-            const pass = document.getElementById('password').value;
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
 
-            if(user === 'admin' && pass === 'admin123') {
-                alert('Log In Successfull!');
-            } else {
-                alert('Invalid Password');
-            }
-        });
+    if (user === 'admin' && pass === 'admin123') {
+        alert('Log In Successful!');
+      
+        window.location.assign('/home.html');
+    } else {
+        alert('Invalid Password! Try again.');
+    }
+});
